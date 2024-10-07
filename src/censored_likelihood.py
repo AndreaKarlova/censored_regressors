81a0795342d66518b4b062084c0e344cb67fe4ac
Andrea Karlova <akarlova@cs.ucl.ac.uk>
2024-10-07 13:13:25 +0100

    Update censored_likelihood.py
    minor bug fixes

--- src/censored_likelihood.py
+++ src/censored_likelihood.py
@@ -7,13 +7,11 @@ import numpy as np
 import torch
 import torch.distributions as dist
 from torch import Tensor
-from .censored_normal import CensoredNormal
-from gpytorch.likelihoods.likelihood import _Likelihood
+from gpytorch.likelihoods.likelihood import Likelihood
 from gpytorch.likelihoods.noise_models import HomoskedasticNoise
 
 
-
-class CensoredGaussianLikelihood(_Likelihood):
+class CensoredGaussianLikelihood(Likelihood):
     """Base class for Censored Gaussian Likelihoods."""
     def __init__(self, variance=None, low=None, high=None) -> None:
         super().__init__()
@@ -32,8 +30,9 @@ class CensoredGaussianLikelihood(_Likelihood):
         return CensoredNormal(function_samples, scale=self.variance.sqrt(), low=self.low, high=self.high)
 
 
+
 class CensoredGaussianLikelihoodAnalytic(CensoredGaussianLikelihood):
-    def __init__(self, variance=None, low=None, high=None, alpha=1., gamma=1., dzeta=1., jitter_=1e-12) -> None:
+    def __init__(self, variance=None, low=None, high=None, alpha=1., gamma=1., dzeta=1., jitter_=1e-32) -> None:
         super().__init__(variance, low, high)
         self.alpha = alpha
         self.gamma = gamma
@@ -50,39 +49,31 @@ class CensoredGaussianLikelihoodAnalytic(CensoredGaussianLikelihood):
         upper_censor = lambda x, s: (self.high - x) / s
         lower_censor = lambda x, s: (self.low - x) / s
 
-        standard_normal = dist.Normal(0,1)
+        standard_normal = dist.Normal(0., 1.)
 
-        #print(noise, sigma, mean, variance, std)
-
+        # continuous part --> this should be reasonably numerically stable
          # Gaussian term
         res = ((target - mean).square() + variance) / noise + noise.log() + math.log(2 * math.pi)
         res = res.mul(-0.5)
         uncensored_cdf = standard_normal.cdf(upper_censor(mean, std)) - standard_normal.cdf(lower_censor(mean, std))
         normal_part = res * uncensored_cdf  # maximized
-
-        # upper term
-        x_u = 0 if torch.isinf(torch.Tensor([self.high])) else 0.5 * (self.high - 2*target + mean) * std * noise.reciprocal()  # minimized
+        # upper boundary asymmetry contribution
+        x_u = torch.where(torch.isinf(torch.Tensor([self.high])), 0.,
+                          0.5 * (self.high - 2*target + mean) * std * noise.reciprocal())
         upper_term_pdf = standard_normal.log_prob(upper_censor(mean, std)).exp()
-        upper_censored_part_cont = x_u * upper_term_pdf
+        upper_censored_part_cont = x_u * upper_term_pdf # minimized
+        # lower boundery asymmetry contribution
+        x_l = torch.where(torch.isinf(torch.Tensor([self.low])), 0.,
+                          0.5 * (self.low - 2*target + mean) * std * noise.reciprocal())
+        lower_term_pdf = standard_normal.log_prob(lower_censor(mean, std)).exp()
+        lower_censored_part_cont =  x_l * lower_term_pdf # maximized
 
         upper_term_cdf = standard_normal.cdf(-upper_censor(target, sigma))
-        # if torch.isinf(self.high) or upper_term_cdf < self.jitter_:
-        #   upper_censored_part_atom = 0.
-        # else:
-        ln_cdf = torch.clamp_min(upper_term_cdf, self.jitter_).log()
+        ln_cdf = torch.log(torch.where(upper_term_cdf < self.jitter_, self.jitter_, upper_term_cdf))
         upper_censored_part_atom = ln_cdf * upper_term_pdf  # minimized
 
-        # lower term
-        x_l = 0 if torch.isinf(torch.Tensor([self.low])) else 0.5 * (self.low - 2*target + mean) * std * noise.reciprocal()  # maximized
-        lower_term_pdf = standard_normal.log_prob(lower_censor(mean, std)).exp()
-        lower_censored_part_cont =  x_l * lower_term_pdf
-
         lower_term_cdf = standard_normal.cdf(lower_censor(target, sigma))
-        # if torch.isinf(self.low):
-        #   lower_censored_part_atom = torch.zero(lower_term_cdf.shape)
-        # else:
-
-        ln_cdf = torch.clamp_min(lower_term_cdf, self.jitter_).log()
+        ln_cdf = torch.log(torch.where(lower_term_cdf < self.jitter_, self.jitter_, lower_term_cdf))
         lower_censored_part_atom = ln_cdf * lower_term_pdf  # minimized
 
         Phi = lambda m, s, a: torch.special.erf((a - m) / (np.sqrt(2) * s))
